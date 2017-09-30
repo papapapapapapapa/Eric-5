c2bc55be319bba89f4cea353fdc39b8306e4c9c5
papapapapapapapa <r3386588@gmail.com>
2017-09-30 15:05:42 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int x,y;
+        x=in.nextInt();
+        y=in.nextInt();
+        System.out.println(x/2.54);
+        System.out.println(y/0.454);
+    }
+}
